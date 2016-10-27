2b000a1f8e565cc0c8d507961a08c03df8d25754
AlexRedchets <alexey.redchets@gmail.com>
2016-10-27 18:49:57 -0400

    Initial commit

---
+++ app/src/main/java/com/azvk/bluetooth/MainActivity.java
@@ -0,0 +1,40 @@
+package com.azvk.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null){
+            Toast.makeText(this, "Device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != 0) {
+            Toast.makeText(this, "Bluetooth was enabled", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Bluetooth was not enabled", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
